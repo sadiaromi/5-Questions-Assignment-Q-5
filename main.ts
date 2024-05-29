3a78c5b0966c7862ff04a96b97f7ba93276e3d28
sadiaromi <sadiaroman28@gmail.com>
2024-05-29 19:15:56 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,9 @@
+//..........................Q# 5..........................
+
+// function `displayMessage()` prints "Hello world"
+// string using console.log() function
+
+function displayMessage(){
+    return "Hello world"
+};
+console.log(displayMessage());
